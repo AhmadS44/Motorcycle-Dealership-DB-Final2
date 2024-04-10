53a1f30aea612c428380d1ac384c17165e17dd18
Ahmad <ac145088@avcol.school.nz>
2024-04-10 20:48:50 +1200

    finished adding motorcycles validations.

--- Motorcycle Dealership DB Final2/Models/Motorcycle.cs
+++ Motorcycle Dealership DB Final2/Models/Motorcycle.cs
@@ -5,13 +5,22 @@ namespace Motorcycle_Dealership_DB_Final2.Models
     public class Motorcycle
     {
         public int MotorcycleID { get; set; }
-       
+
+        [Required]
+        [StringLength(25)]
         public string Model { get; set; }
-       
+
+        [Required]
+        [Range(1900, 2024, ErrorMessage = "Please enter a vaild year (1900 - 2024)")]
+        [Display(Name = "Year")]
         public int Year { get; set; }
-        
+
+        [Required]
+        [StringLength(10)]
         public string Weight { get; set; }
-        
+
+        [Required]
+        [StringLength(10)]
         public string Colour { get; set; }
 
 
