c5bc1c80af07d7d9e319d3acc5f37950dd330302
Ahmad <ac145088@avcol.school.nz>
2024-04-10 20:54:45 +1200

    finished adding purchaseorders validations.

--- Motorcycle Dealership DB Final2/Models/PurchaseOrder.cs
+++ Motorcycle Dealership DB Final2/Models/PurchaseOrder.cs
@@ -6,13 +6,24 @@ namespace Motorcycle_Dealership_DB_Final2.Models
     {
         public int PurchaseOrderID { get; set; }
         public int CustomerID { get; set; }
-        
+
+        [Display(Name = "PurchaseDate")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime PurchaseDate { get; set; }
-        
+
+        [Required]
+        [StringLength(25)]
         public string Model { get; set; }
-        
+
+        [Required]
+        [Range(1000, 9999, ErrorMessage = "Please enter a vaild zip code (1000 - 9999)")]
+        [Display(Name = "Zip")]
         public int Zip { get; set; }
-        
+
+        [Required]
+        [Range(0210000000, 0219999999, ErrorMessage = "Please enter a vaild phone number (021-0000000)")]
+        [Display(Name = "PhoneNumber")]
         public int PhoneNumber {  get; set; }  
 
 
